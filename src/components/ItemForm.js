27e77c5b29232d2da537ef6f2b7b8ef8faa0f4a0
Irene Gitau <irene.gitau@strathmore.edu>
2023-10-30 15:33:53 +0300

    done

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -3,9 +3,15 @@ import React, { useState } from "react";
 function ItemForm() {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("name:", name);
+    console.log("category:", category);
+  }
   return (
-    <form className="NewItem">
+    <form className="NewItem"> <form className="NewItem" onSubmit={handleSubmit}>
+    {/** ...form inputs here */}
+  </form>
       <label>
         Name:
         <input
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
-
+import { useEffect, useState } from "react";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
-
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((r) => r.json())
+      .then((items) => console.log(items));
+  }, []);
   function handleCategoryChange(category) {
     setSelectedCategory(category);
   }
